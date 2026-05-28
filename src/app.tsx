53d33738e90ee116314c0a178873cae3a8f3c802
ysdede <ysdede@duck.com>
2026-05-28 21:04:20 +0300

    fix: add MetaProvider wrapper for @solidjs/meta

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,13 @@
+import { MetaProvider } from '@solidjs/meta'
 import { Router, Route } from '@solidjs/router'
 import Home from './routes/index'
 
 export default function App() {
   return (
-    <Router root={(props) => props.children}>
-      <Route path="/" component={Home} />
-    </Router>
+    <MetaProvider>
+      <Router root={(props) => props.children}>
+        <Route path="/" component={Home} />
+      </Router>
+    </MetaProvider>
   )
 }
